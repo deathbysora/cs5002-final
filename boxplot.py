4c97350910978e599c99fe60502f7326eff69ec6
philipquan <quan.p@northeastern.edu>
2022-05-02 00:30:33 -0700

    Writes data to text file

--- boxplot.py
+++ boxplot.py
@@ -1,12 +1,66 @@
-import os
-import re
-
 import pandas
+import seaborn
+from matplotlib import pyplot
 
 
-def main() -> None:
-    source_path = "data\\cities\\"
-    for file_name in os.listdir(source_path):
-        print(f"beginning operation on {file_name}")
-        airport = re.findall(r"[A-Z]{3}", file_name)[0]
-        data = pandas.read_csv(source_path + file_name)
+def boxplot_data(xcolumn: str, ycolumn: str) -> None:
+    path = "data\\"
+    data = pandas.read_csv(path + "weather.csv")
+    destination_path = "graphs\\"
+    file_path = destination_path + xcolumn + "x" + ycolumn
+
+    lbound = data[ycolumn].quantile(0.05)
+    ubound = data[ycolumn].quantile(0.95)
+
+    data = data[(data[ycolumn] > lbound) & (data[ycolumn] < ubound)]
+
+    seaborn.set_palette(seaborn.color_palette("Spectral"))
+    seaborn.boxplot(x=xcolumn, y=ycolumn, data=data)
+    pyplot.savefig(file_path + ".png")
+    pyplot.clf()
+
+    airports = data[xcolumn].unique()
+    with open(file_path + ".txt", "w") as file:
+        for airport in airports:
+            filtered = data[data[xcolumn] == airport]
+            series = filtered[ycolumn]
+
+            file.write(airport + "\n")
+            file.write("-" * 10 + "\n")
+
+            collected = {
+                "count": series.count(),
+                "mean": series.mean(),
+                "min": series.min(),
+                "std": series.std(),
+                "25%": series.quantile(0.25),
+                "50%": series.quantile(0.50),
+                "75%": series.quantile(0.75),
+                "max": series.max()
+            }
+
+            for label, value in collected.items():
+                file.write(f"{label}: {value}\n")
+            file.write("\n\n")
+
+    print(f"Box plot saved to {file_path}")
+
+
+def main():
+    xs = [
+        "ORIGIN",
+        "DEST",
+    ]
+
+    ys = [
+        "DEP_DELAY",
+        "ARR_DELAY",
+    ]
+
+    for x in xs:
+        for y in ys:
+            boxplot_data(x, y)
+
+
+if __name__ == "__main__":
+    main()
