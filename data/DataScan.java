15b0911be6456809e5662c0d70583e6ae8989778
philipquan <quan.p@northeastern.edu>
2022-05-01 19:57:16 -0700

    Added weather data

---
+++ data/DataScan.java
@@ -0,0 +1,73 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+/**
+ * This class process the data and filter out any of the unnecessary entries.
+ */
+public class DataScan {
+  /*
+   * The  city should only contain: SEA JFK AUS HNL MIA LAX
+   * The fields should only contain:
+   *    date (FL_DATE), airline (OP_CARRIER), flight number (OP_CARRIER_FL_NUM)
+   *    Departure city (ORIGIN), destination (DEST),
+   *    departure time (DEP_TIME), DEP_DELAY, arrival time (ARR_TIME), ARR_DELAY
+   *    [0, 1, 2, 3, 5, 8, 13]
+   */
+  private static final int[] REQUIRED_INDEX = new int[] {0, 1, 2, 3, 4, 6, 7, 11, 12};
+  private static final String[] REQUIRED_CITY = new String[]{"SEA", "JFK", "AUS", "HNL", "MIA", "LAX"};
+  
+  public static void main(String[] args) throws FileNotFoundException {
+    
+    // set up file systems
+    File allEntries = new File("2019_No_Cities.csv");
+    PrintStream output = new PrintStream(new File("sortedData.csv"));
+    Scanner entryScan = new Scanner(allEntries);
+  
+  
+    String[] line = entryScan.nextLine().split(",");
+    output.println(discardLines(line));
+    
+    // loop through all files
+    while (entryScan.hasNext()) {
+      // scan and write the first line
+      line = entryScan.nextLine().split(",");
+      
+      // check if the plane is diverted
+      int cancelledCode = Integer.parseInt(line[13]);
+      boolean isCancelled;
+      
+      // check if the origin and destination is the required city
+      if (checkCities(line[3]) && checkCities(line[4]) && cancelledCode == 0) {
+        output.println(discardLines(line));
+        //System.out.println(discardLines(line));
+        
+      }
+    }
+    
+  }
+  
+  private static String discardLines(String[] entry) {
+    
+    String output = entry[REQUIRED_INDEX[0]];
+    
+    // loop through and construct
+    for (int i = 1; i < REQUIRED_INDEX.length; i++) {
+      output += "," + entry[REQUIRED_INDEX[i]];
+    }
+    
+    return output;
+  }
+  
+  private static boolean checkCities(String city) {
+    
+    for (String c : REQUIRED_CITY) {
+      if (city.equals(c)) {
+        return true;
+      }
+    }
+    
+    return false;
+  }
+}
---
+++ data/IncludeWeather.java
@@ -0,0 +1,256 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * This class reads each city and add the weather data of the destination and weather.
+ */
+public class IncludeWeather {
+  
+  /*
+   * This class will loop though each entry. add visibility, wind speed to both origin and
+   * destination at the time of the departure/arrival.
+   */
+  
+  private static final int[] WEATHER_INDEX = new int[] {2, 4};
+  
+  private static Map<String, String[]> seaWeather;
+  private static Map<String, String[]> ausWeather = new HashMap<>();
+  private static Map<String, String[]> laxWeather = new HashMap<>();
+  private static Map<String, String[]> jfkWeather = new HashMap<>();
+  private static Map<String, String[]> miaWeather = new HashMap<>();
+  private static Map<String, String[]> hnlWeather = new HashMap<>();
+  
+  public static void main(String[] args) throws FileNotFoundException {
+    
+    // scan and build the weather data first
+    File dir = new File("weather");
+    File dirListing[] = dir.listFiles();
+    
+    File weather = new File ("weather/LAX_weather.csv");
+    
+    // Data structure:
+    // Key: Date in form M/D/YY.
+    // Value: String[] {Wind speed, Visibilities}
+    seaWeather = new HashMap<>();
+    ausWeather = new HashMap<>();
+    laxWeather = new HashMap<>();
+    jfkWeather = new HashMap<>();
+    miaWeather = new HashMap<>();
+    hnlWeather = new HashMap<>();
+    
+    if (dirListing != null) {
+      
+      for (File currentFile : dirListing) {
+  
+        Scanner weatherScan = new Scanner(currentFile);
+        weatherScan.next();
+        
+        while (weatherScan.hasNext()) {
+          String[] tokens = weatherScan.nextLine().split(",");
+    
+          switch (tokens[0]) {
+            case "LAX":
+              laxWeather.put(processDateWeather(tokens[1], false), new String[]{tokens[2], tokens[4]});
+              break;
+            case "SEA":
+              seaWeather.put(processDateWeather(tokens[1], false), new String[]{tokens[2], tokens[4]});
+              break;
+            case "AUS":
+              ausWeather.put(processDateWeather(tokens[1], false), new String[]{tokens[2], tokens[4]});
+              break;
+            case "JFK":
+              jfkWeather.put(processDateWeather(tokens[1], false), new String[]{tokens[2], tokens[4]});
+              break;
+            case "MIA":
+              miaWeather.put(processDateWeather(tokens[1], false), new String[]{tokens[2], tokens[4]});
+              break;
+            case "PHNL":
+              hnlWeather.put(processDateWeather(tokens[1], true), new String[]{tokens[2], tokens[4]});
+              break;
+            default:
+              System.out.println("No line found");
+          }
+    
+        }
+        
+      }
+    }
+    
+    // set up file systems
+    File allEntries = new File("sortedData.csv");
+    PrintStream output = new PrintStream(new File("sortedData_weather.csv"));
+    Scanner entryScan = new Scanner(allEntries);
+    
+    // printout titles
+    output.println(entryScan.nextLine()
+            + ",ORIGIN_WIND_SPEED,ORIGIN_VISIBILITIES,DEST_WIND_SPEED,DEST_VISIBILITIES");
+    
+    // scan through the rest of the data and add in the weather data
+    while (entryScan.hasNext()) {
+      //String[] tokens = entryScan.nextLine().split(",");
+      String test = addWeatherData(entryScan.nextLine());
+      //System.out.println();
+      output.println(test);
+    }
+  }
+  
+  // it gets the input from sortedData regards to departure time/city, arrival time/city
+  // and return (4) added data regards to:
+  // Origin wind speed, origin visibilities, destination wind speed, destination visibilities
+  private static String addWeatherData(String scan) {
+    String output = scan; // original entry
+    //System.out.println(scan);
+    
+    String[] tokens = scan.split(","); // break up the values
+    
+    //String[] datePlueMinute = tokens[0].split(" ");
+    String[] date = tokens[0].split(" ")[0].split("/");
+    
+    
+    String originCity = tokens[3];
+    String destinationCity = tokens[4];
+    
+    String departureTime;
+    String arrivalTime;
+    
+    // round up time to the nearest 5
+    try {
+      departureTime = roundUpOrDow(tokens[5]);
+      arrivalTime = roundUpOrDow(tokens[7]);
+    } catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println("Incomplete Data!");
+      return "";
+    }
+    
+    // loop up the weather data from the maps
+    String departureKey = tokens[0].split(" ")[0] + "," + departureTime;
+    String arrivalKey = tokens[0].split(" ")[0] + "," + arrivalTime;
+    
+    String departureValue = weatherLookup(departureKey, originCity);
+    String arrivalValue = weatherLookup(arrivalKey, destinationCity);
+    
+    
+    return output + "," + departureValue + "," + arrivalValue;
+  }
+  
+  private static String weatherLookup(String key, String city) {
+    
+    String output = ",";
+    String[] value = null;
+  
+    try {
+      switch (city) {
+        case "LAX":
+          value = laxWeather.get(key);
+          return value[0] + "," + value[1];
+    
+        case "SEA":
+          value = seaWeather.get(key);
+          return value[0] + "," + value[1];
+        //break;
+        case "AUS":
+          value = ausWeather.get(key);
+          return value[0] + "," + value[1];
+        //break;
+        case "JFK":
+          value = jfkWeather.get(key);
+          return value[0] + "," + value[1];
+        //break;
+        case "MIA":
+          value = miaWeather.get(key);
+          return value[0] + "," + value[1];
+        //break;
+        case "PHNL":
+          value = hnlWeather.get(key);
+          return value[0] + "," + value[1];
+        //break;
+        default:
+          System.out.println("No line found");
+      }
+    } catch (NullPointerException e) {
+      System.out.println("No weather value found");
+    }
+    
+    return output;
+  }
+  
+  // process date format xx/xx/xx/ 00:00
+  private static String processDateWeather(String inputDate, boolean isHnl) {
+    
+    String output = "";
+    
+    String[] datePlusMinute  = inputDate.split(" ");
+    String[] time = datePlusMinute[1].split(":");
+    String[] dateInput = datePlusMinute[0].split("-");
+    
+    String datePrint = Integer.parseInt(dateInput[1]) + "/" + Integer.parseInt(dateInput[2]) + "/" + dateInput[0].substring(2);
+    int hourAdd;
+    String hawaiiHour;
+    
+    if (isHnl) {
+      int hnlHour = Integer.parseInt(time[0]) - 10;
+      hourAdd = Integer.parseInt(hnlHour + time[1]);
+    } else {
+      hourAdd = Integer.parseInt(time[0] + time[1]);
+    }
+    
+    output = datePrint + "," + hourAdd;
+    
+    return output;
+  }
+  
+  private static int[] getDate(String dateEntry) {
+    // incoming format shall be 4/1/19 0:00
+    
+    String[] splitData = dateEntry.split(" ")[0].split("/");
+    
+    return new int[] {Integer.parseInt(splitData[0]),
+            Integer.parseInt(splitData[1]), Integer.parseInt(splitData[2])};
+    
+  }
+  
+  private static String roundUpOrDow(String timeEntry) {
+    
+    int time =  Integer.parseInt(timeEntry);
+    int hour = time / 100;
+    int minute = time % 100;
+    
+    int mod = minute % 5;
+    
+    if (mod < 3) {
+      minute = minute - mod;
+    } else {
+      minute = minute + (5 - mod);
+    }
+    
+    if (minute > 59) {
+      minute = 0;
+      hour++;
+      
+      if (hour == 24) {
+        return 23 + "" + 55;
+      }
+      
+      return hour + "00";
+    }
+    
+    if (hour != 0) {
+      if (minute / 10 == 0) {
+        return hour + "0" + minute;
+      } else {
+        return hour + "" + minute;
+      }
+    } else {
+      if (minute / 10 == 0) {
+        return "0" + minute;
+      } else {
+        return "" + minute;
+      }
+    }
+  }
+}
