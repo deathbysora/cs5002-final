ae16d5390699e8cbabba44530cfe4155ecd4169e
philipquan <quan.p@northeastern.edu>
2022-05-01 19:56:03 -0700

    Moved global

--- app.py
+++ app.py
@@ -5,14 +5,13 @@ from pandas import read_csv
 
 from data_plotter import DataPlotter
 
-SOURCE_PATH = "data\\cities\\"
-
 
 def main() -> None:
-    for file_name in os.listdir(SOURCE_PATH):
+    source_path = "data\\cities\\"
+    for file_name in os.listdir(source_path):
         print(f"beginning operation on {file_name}")
         airport = re.findall(r"[A-Z]{3}", file_name)[0]
-        handler = DataPlotter(airport, read_csv(SOURCE_PATH + file_name))
+        handler = DataPlotter(airport, read_csv(source_path + file_name))
         handler.run()
 
 
