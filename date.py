3e93f2ce0d7199acefdbcadbc7b76b325e23cd94
philipquan <quan.p@northeastern.edu>
2022-05-03 02:50:59 -0700

    New files for generating graphs

---
+++ date.py
@@ -0,0 +1,58 @@
+import seaborn
+from matplotlib import dates, pyplot
+from pandas import read_csv
+from scipy import stats
+
+
+def main():
+    with open("data\\cities\\combined_v3.csv", "r") as file:
+        data = read_csv(file)
+
+    ykey = "DEP_DELAY"
+    xkey = "FL_DATE"
+
+    destination = "graphs\\"
+    file_name = "".join([destination, ykey, "x", xkey])
+    stream = open(file_name + ".txt", "w")
+    stream.write(
+        "data is linear regression model calculated from scipy for each airport\n")
+    stream.write(f"y = {ykey}\n")
+    stream.write(f"x = {xkey}\n")
+    stream.write("\n")
+
+    airports = data["ORIGIN"].unique()
+    for airport in airports:
+        filtered = data[data["ORIGIN"] == airport]
+        y = filtered[ykey].to_list()
+        x = filtered[xkey].to_list()
+        x = list(map(lambda x: int(x.replace("-", "")) - 20190000, x))
+
+        slope, intercept, r, p, std_err = stats.linregress(x, y)
+        results = {
+            "count": len(filtered),
+            "slope": slope,
+            "intercept": intercept,
+            "r": r,
+            "p": p,
+            "std error": std_err,
+        }
+
+        stream.write(airport + "\n")
+        for key, value in results.items():
+            stream.write(f"{key}: {value}\n")
+        stream.write("\n\n")
+
+        pyplot.figure(figsize=(11, 8))
+        pyplot.gca().xaxis.set_major_formatter(dates.DateFormatter("%b"))
+        pyplot.gca().xaxis.set_major_locator(dates.MonthLocator())
+        pyplot.gcf().autofmt_xdate()
+
+        plot = seaborn.scatterplot(x=xkey, y=ykey, data=filtered)
+        plot.set_title(airport + " 2019")
+
+        pyplot.savefig(file_name + "_" + airport + ".png")
+        pyplot.clf()
+
+
+if __name__ == "__main__":
+    main()
---
+++ time.py
@@ -0,0 +1,57 @@
+import seaborn
+from matplotlib import pyplot
+from pandas import read_csv
+from scipy import stats
+
+
+def main(xkey: str):
+    with open("data\\cities\\combined_v3.csv", "r") as file:
+        data = read_csv(file)
+
+    ykey = "DEP_DELAY"
+
+    destination = "graphs\\"
+    file_name = "".join([destination, ykey, "x", xkey])
+    stream = open(file_name + ".txt", "w")
+    stream.write(
+        "data is linear regression model calculated from scipy for each airport\n")
+    stream.write(f"y = {ykey}\n")
+    stream.write(f"x = {xkey}\n")
+    stream.write("\n")
+
+    airports = data["ORIGIN"].unique()
+    for airport in airports:
+        filtered = data[data["ORIGIN"] == airport]
+        y = filtered[ykey].to_list()
+        x = filtered[xkey].to_list()
+
+        slope, intercept, r, p, std_err = stats.linregress(x, y)
+        results = {
+            "count": len(filtered),
+            "slope": slope,
+            "intercept": intercept,
+            "r": r,
+            "p": p,
+            "std error": std_err,
+        }
+
+        stream.write(airport + "\n")
+        for key, value in results.items():
+            stream.write(f"{key}: {value}\n")
+        stream.write("\n\n")
+
+        pyplot.figure(figsize=(11, 8))
+        plot = seaborn.scatterplot(x=xkey, y=ykey, data=filtered)
+        plot.set_title(airport + " 2019")
+
+        pyplot.savefig(file_name + "_" + airport + ".png")
+        pyplot.clf()
+
+
+if __name__ == "__main__":
+    xkeys = [
+        "ARR_TIME",
+        "DEP_TIME",
+    ]
+    for xkey in xkeys:
+        main(xkey)
