a4924587b62533e1fd0debef7908ffcc76f235ed
philipquan <quan.p@northeastern.edu>
2022-05-03 03:51:15 -0700

    Created delay reason bar and pie chart script

--- delay_reason.py
+++ delay_reason.py
@@ -28,20 +28,43 @@ def filter_reasons(data: DataFrame) -> Dict[str, int]:
     return result
 
 
+def graph_bar(airport: str, data: Dict[str, int]):
+    pyplot.figure(figsize=(11, 8))
+    plot = seaborn.barplot(x=list(data.keys()),
+                           y=list(data.values()))
+    plot.set_title(airport + " Delay Reasons 2019")
+    plot.bar_label(plot.containers[0])
+    pyplot.savefig("graphs\\DELAY_REASONS_BAR_" + airport + ".png")
+
+
+def graph_pie(airport: str, data: Dict[str, int]):
+    pyplot.figure(figsize=(11, 8))
+    pyplot.pie(list(data.values()),
+               labels=list(data.keys()),
+               autopct="%.2f%%")  # type: ignore
+    pyplot.title(airport + " Delay Reasons 2019")
+    pyplot.savefig("graphs\\DELAY_REASONS_PIE_" + airport + ".png")
+
+
 def main():
     with open("data\\cities\\combined_v3.csv", "r") as file:
         data = read_csv(file)
+
+    # combined
+    result = filter_reasons(data)  # type: ignore
+    graph_bar("COMBINED", result)
+    pyplot.clf()
+    graph_pie("COMBINED", result)
+    pyplot.clf()
 
     airports = data["ORIGIN"].unique()
     for airport in airports:
         filtered = data[data["ORIGIN"] == airport]
         result = filter_reasons(filtered)  # type: ignore
-        pyplot.figure(figsize=(11, 8))
-        plot = seaborn.barplot(x=list(result.keys()),
-                               y=list(result.values()))
-        plot.set_title(airport + " Delay Reasons 2019")
-        plot.bar_label(plot.containers[0])
-        pyplot.savefig("graphs\\DELAY_REASONS_" + airport + ".png")
+        graph_bar(airport, result)
+        pyplot.clf()
+        graph_pie(airport, result)
+        pyplot.clf()
 
 
 main()
