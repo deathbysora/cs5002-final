74cf36e2ab13a0ad0c17af48911111f16be25573
philipquan <quan.p@northeastern.edu>
2022-05-03 20:42:16 -0700

    Changed to combinedv3

--- helper/concat_csv.py
+++ helper/concat_csv.py
@@ -1,15 +1,16 @@
-"""Combines csv's into one."""
+"""Combines all csv files in a directory into one csv."""
 import os
+
 import pandas
 
 
 def main():
     """Entry Point."""
-    source = os.getcwd() + r"\source"
-    file_names = [source + "\\" + name for name in os.listdir(source)]
-    combined_csv = pandas.concat([pandas.read_csv(file)
-                                 for file in file_names])
-    combined_csv.to_csv("combined_data.csv", index=False)
+    source = "data\\source\\"
+    file_paths = [source + name for name in os.listdir(source)]
+    combined = pandas.concat([pandas.read_csv(file_path)
+                              for file_path in file_paths])
+    combined.to_csv(source + "combined_data.csv", index=False)
 
 
 if __name__ == "__main__":
